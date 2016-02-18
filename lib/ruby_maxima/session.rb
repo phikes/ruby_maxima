a6f230ca3569ac6404ca329886a4b55741c5fb6b
Phillip Kessels <info@phikes.de>
2016-02-18 14:01:35 +0100

    Can add commands with arguments now

--- lib/ruby_maxima/session.rb
+++ lib/ruby_maxima/session.rb
@@ -1,4 +1,13 @@
 module RubyMaxima
   class Session
+    attr_reader :commands
+
+    def initialize
+      @commands = []
+    end
+
+    def method_missing(method, *args, &block)
+      @commands << "#{method}(#{args.join ','})"
+    end
   end
 end
--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -4,4 +4,14 @@ describe RubyMaxima::Session do
   it 'can be created' do
     expect(subject).not_to be nil
   end
+
+  it 'allows for adding single argument commands' do
+    subject.sqrt 2
+    expect(subject.commands).to include 'sqrt(2)'
+  end
+
+  it 'allows for adding multiple argument commands' do
+    subject.sqrt 2, 3
+    expect(subject.commands).to include 'sqrt(2,3)'
+  end
 end
